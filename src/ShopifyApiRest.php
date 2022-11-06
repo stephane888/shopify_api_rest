dc2fd0ab08aed2e08963703c7cac5a04ab6696aa
kouwa stephane <kksasteph888@gmail.com>
2022-11-06 21:23:35 +0100

    Maj

--- src/ShopifyApiRest.php
+++ src/ShopifyApiRest.php
@@ -9,10 +9,21 @@ namespace Drupal\shopify_api_rest;
  */
 class ShopifyApiRest {
   
+  /**
+   *
+   * @see https://shopify.dev/api/usage/access-scopes
+   * @return string[]
+   */
   static public function listGrantOption() {
     return [
+      "read_all_orders" => "read_all_orders",
       "write_products" => "write_products",
-      "read_shipping" => "read_shipping"
+      "read_products" => "read_products",
+      "read_shipping" => "read_shipping",
+      "read_content" => "read_content",
+      "write_content" => "write_content",
+      "read_checkouts" => "read_checkouts",
+      "write_checkouts" => "write_checkouts"
     ];
   }
   
